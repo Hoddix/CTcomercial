2de2f5f62b1b3afa407d33b59282248dc8bf9eab
Javier <hoddix@hotmail.com>
2015-03-13 22:49:23 +0100

    Delete config_db.php

--- php/config_db.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    define('DB_HOST','mysql.hostinger.es'); 
-    define('DB_USER','u807027470_demo'); 
-    define('DB_PASS','pollete'); 
-    define('DB_NAME','u807027470_demo'); 
-    define('DB_CHARSET','utf8'); 
-?>
